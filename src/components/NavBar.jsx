74d3a523d30a01d3f92d47015a4452a19b799b20
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-14 20:35:03 +1000

    created basic navbar components with default materialUI syntax

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {AppBar, Toolbar, Typography, Tabs, Tab} from '@mui/material';
+import {Link} from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <AppBar>
+      <Typography variant="h2">
+        <Toolbar>
+          <Tabs>
+            <Tab label="Home" value="/events" component={Link} to="/events" />
+            <Tab label="Food Trucks" value="/foodtrucks" component={Link} to="/foodtrucks" />
+            <Tab label="Breweries" value="/breweries" component={Link} to="/breweries" />
+            <Tab label="Sign In" value="/auth/signin" component={Link} to="/auth/signin" />
+            <Tab label="Sign Up" value="/auth/signup" component={Link} to="/auth/signup" />
+            <Tab label="Log Out" component={Link} to="/events" />
+          </Tabs>
+        </Toolbar>
+      </Typography>
+    </AppBar>
+    // <nav>
+    //   <Link to="/events">Home</Link>
+    //   <Link to="/foodtrucks">Food Trucks</Link>
+    //   <Link to="/breweries">Breweries</Link>
+    //   <Link to="/auth/signin">Sign In</Link>
+    //   <Link to="/auth/signup">Sign Up</Link>
+    //   <Link to="/events">Logout</Link>
+    // </nav>
   );
 }
 
