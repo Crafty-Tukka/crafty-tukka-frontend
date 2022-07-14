cfadf7f6e75b97ea4f6a07c7657859b957b23e98
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-14 13:46:53 +1000

    renamed venue.jsx to Venue.jsx. Created and exported functional components in each component .jsx file

---
+++ src/components/food-trucks/FoodTrucks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FoodTrucks() {
+  return (
+    <div>
+      <h1>FoodTrucks</h1>
+    </div>
+  );
+}
+
+export default FoodTrucks;
---
+++ src/components/venues/Venues.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Venues() {
+  return (
+    <div>
+      <h1>Venues</h1>
+    </div>
+  );
+}
+
+export default Venues;
