37d130000bf87022ea5f72338a70da5568b8928d
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-15 10:54:04 +1000

    added additonal links to navbar. all links working except my events tab that requires route and logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography, Tabs, Tab} from '@mui/material';
+import {AppBar, Toolbar, Typography, Tabs, Tab, Container} from '@mui/material';
 import {Link} from 'react-router-dom';
 
 function NavBar() {
   return (
     <>
-      <AppBar position="absolute">
-        <Typography variant="h2">
-          <Toolbar>
-            <Tabs value={false}>
-              <Tab label="Home" value="/events" component={Link} to="/events" />
-              <Tab label="Food Trucks" value="/foodtrucks" component={Link} to="/foodtrucks" />
-              <Tab label="Breweries" value="/venues" component={Link} to="/venues" />
-              <Tab label="Sign In" value="/auth/signin" component={Link} to="/auth/signin" />
-              <Tab label="Sign Up" value="/auth/signup" component={Link} to="/auth/signup" />
-              <Tab label="Log Out" component={Link} to="/events" />
-            </Tabs>
-          </Toolbar>
-        </Typography>
+      <AppBar position="sticky">
+        <Container maxWidth="xl">
+          <Typography variant="h2">
+            <Toolbar disableGutters>
+              <Tabs value={false}>
+                <Tab label="Home" value="/events" component={Link} to="/events" />
+                <Tab label="Food Trucks" value="/foodtrucks" component={Link} to="/foodtrucks" />
+                <Tab label="Breweries" value="/venues" component={Link} to="/venues" />
+                <Tab label="Sign In" value="/auth/signin" component={Link} to="/auth/signin" />
+                <Tab label="Sign Up" value="/auth/signup" component={Link} to="/auth/signup" />
+                <Tab label="My Events" />
+                <Tab label="Book an Event" value="/events/new" component={Link} to="/events/new" />
+                <Tab
+                  label="Edit Profile"
+                  value="auth/editprofile"
+                  component={Link}
+                  to="auth/editprofile"
+                />
+                <Tab label="Log Out" component={Link} to="/events" />
+              </Tabs>
+            </Toolbar>
+          </Typography>
+        </Container>
       </AppBar>
     </>
 
