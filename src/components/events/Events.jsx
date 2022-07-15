2efcdf03436b6947ce25f27919c8ac2770bdc758
jwsandeman <jw.sandeman@gmail.com>
2022-07-15 15:27:14 +1000

    imported confirmed events from global state and added map logic to render each confirmed event in a list the sidebar

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,8 +1,13 @@
 import {useMemo, useCallback, useRef} from 'react';
 import {GoogleMap} from '@react-google-maps/api';
 import './EventStyle.css';
+import {useGlobalState} from 'utils/stateContext';
 
 function Events() {
+  const {store} = useGlobalState();
+  const {confirmedEvents} = store;
+  console.log(confirmedEvents);
+
   const center = useMemo(() => ({lat: -27.4705, lng: 153.026}), []);
   const options = useMemo(
     () => ({
@@ -18,6 +23,16 @@ function Events() {
     <div className="container">
       <div className="controls">
         <h1>Event List</h1>
+        {confirmedEvents.map((event) => {
+          return event.confirmed_status === 'confirmed' ? (
+            <>
+              <h3>Name: {event.name}</h3>
+              <p>Description: {event.description}</p>
+              <h5>Start Time: {event.start}</h5>
+              <h5>Finish Time: {event.finish}</h5>
+            </>
+          ) : null;
+        })}
       </div>
       <div className="map">
         <GoogleMap
