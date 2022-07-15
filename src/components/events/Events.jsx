a1eea2d32b29adc7cc168eaad740ef970f301843
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-15 12:13:13 +1000

    map libraries imported. started scaffold code written to render map on events page

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import {useMemo, useCallback, useRef} from 'react';
+import {GoogleMap} from '@react-google-maps/api';
+import './EventStyle.css';
 
 function Events() {
+  const center = useMemo(() => ({lat: -27.4705, lng: 153.026}), []);
+  const options = useMemo(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true
+    }),
+    []
+  );
+  const mapRef = useRef(GoogleMap);
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+
   return (
-    <div>
-      <h1>Events</h1>
+    <div className="container">
+      <div className="controls">
+        <h1>Event List</h1>
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={12}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        ></GoogleMap>
+      </div>
     </div>
   );
 }
