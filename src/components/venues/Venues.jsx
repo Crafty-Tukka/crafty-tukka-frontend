3f37b5bcfd48301ff6a9491ef51c83c64c7864f2
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-17 08:24:23 +1000

    added code to venues.jsx to display venue details

--- src/components/venues/Venues.jsx
+++ src/components/venues/Venues.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import {useGlobalState} from 'utils/stateContext';
+import List from 'components/List';
+import {Typography} from '@mui/material';
+import PreviewCard from 'components/PreviewCard';
 
 function Venues() {
+  const {store} = useGlobalState();
+  const {venues} = store;
+  console.log(venues);
+
+  const venueImg =
+    'https://cdn.shopify.com/s/files/1/0619/1700/3994/files/Ballistic-Beer-Our-Story-About-Us-1.jpg?crop=top&height=275&v=1650431502&width=275';
   return (
-    <div>
-      <h1>Venues</h1>
-    </div>
+    <List title="Venues">
+      {venues.map((venue) => {
+        return (
+          <PreviewCard key={venue.id} imgPath={venueImg} item={venue} routePath={'venues'}>
+            <Typography component="div" variant="h6">
+              {venue.name}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              {venue.description}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {venue.location.address1}, {venue.location.city}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {venue.facebook}
+            </Typography>
+          </PreviewCard>
+        );
+      })}
+    </List>
   );
 }
 
