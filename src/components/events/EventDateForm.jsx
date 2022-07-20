65ee2fce5d66f7586421eda0b7adbd1d8f330c14
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-20 14:52:16 +1000

    added date picker fields. code not functioning as it will need state for each input

--- src/components/events/EventDateForm.jsx
+++ src/components/events/EventDateForm.jsx
@@ -3,7 +3,9 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
-import {DateTimePicker} from '@mui/x-date-pickers';
+import {Typography} from '@mui/material';
+import {MobileDatePicker} from '@mui/x-date-pickers/MobileDatePicker';
+import {TimePicker} from '@mui/x-date-pickers/TimePicker';
 
 function EventDateForm() {
   const [value, setValue] = React.useState(new Date(''));
@@ -16,8 +18,32 @@ function EventDateForm() {
     <React.Fragment>
       <LocalizationProvider dateAdapter={AdapterMoment}>
         <Stack spacing={3}>
-          <DateTimePicker
-            label="Choose Your Event Date and Time"
+          <Typography variant="h5">Choose your Event Start Date</Typography>
+          <MobileDatePicker
+            label="Event Date Start"
+            inputFormat="DD/MM/yyyy"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <Typography variant="h5">Choose your Start Time</Typography>
+          <TimePicker
+            label="Start Time"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <Typography variant="h5">Choose your Event Finish Date</Typography>
+          <MobileDatePicker
+            label="Event Date Start"
+            inputFormat="DD/MM/yyyy"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <Typography variant="h5">Choose your Finish Time</Typography>
+          <TimePicker
+            label="Finish Time"
             value={value}
             onChange={handleChange}
             renderInput={(params) => <TextField {...params} />}
