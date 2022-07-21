c790fcbc1678d743c4e967d0a5efede62ed8ff14
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-22 09:56:32 +1000

    form rendering. need to add submit button and handle state of date.

--- src/components/events/EventForm.jsx
+++ src/components/events/EventForm.jsx
@@ -1,11 +1,26 @@
 import React, {useState} from 'react';
 import {useGlobalState} from 'utils/stateContext';
 import {useNavigate} from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {Typography} from '@mui/material';
+import {MobileDateTimePicker} from '@mui/x-date-pickers/MobileDateTimePicker';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {FormControl, InputLabel, Select} from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import MenuItem from '@mui/material/MenuItem';
+
+const theme = createTheme();
 
 function EventForm({addEvent}) {
   const initialEventData = {
-    start: Date.new(),
-    end: Date.new(),
+    start: new Date(),
+    end: new Date(),
     truck: ''
   };
 
@@ -32,9 +47,75 @@ function EventForm({addEvent}) {
     });
   };
 
+  console.log(formData);
+
   return (
     <div>
-      <form>EventForm</form>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                Select Your Food Truck
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth>
+                      <InputLabel>Food Truck</InputLabel>
+                      <>
+                        <Select
+                          required
+                          label="Food Truck"
+                          name="truck"
+                          value={formData.truck}
+                          onChange={handleFormData}
+                        >
+                          {foodTrucks.map((truck) => {
+                            return (
+                              <MenuItem key={truck.id} value={truck.name} name={truck.name}>
+                                {truck.name}
+                              </MenuItem>
+                            );
+                          })}
+                        </Select>
+                      </>
+                    </FormControl>
+                  </Grid>
+                </Grid>
+                <Stack spacing={3}>
+                  <Typography variant="h5">Choose your Event Start Date and Time</Typography>
+                  <MobileDateTimePicker
+                    label="Start Date and Time"
+                    inputFormat="DD/MM/yyyy hh:mm a"
+                    name="start"
+                    value={formData.start}
+                    onChange={handleFormData}
+                    // renderInput={(params) => <TextField {...params} />}
+                  />
+                  <Typography variant="h5">Choose your Event End Date and Time</Typography>
+                  <MobileDateTimePicker
+                    label="End Date and Time"
+                    inputFormat="DD/MM/yyyy hh:mm a"
+                    name="end"
+                    value={formData.end}
+                    onChange={handleFormData}
+                    // renderInput={(params) => <TextField {...params} />}
+                  />
+                </Stack>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </LocalizationProvider>
     </div>
   );
 }
