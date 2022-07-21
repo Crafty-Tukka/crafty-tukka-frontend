c0854f979535a05a64be94ffa8c7ffbaaa066169
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-22 09:36:47 +1000

    added addEvent method in app.js. to be refactoder to use reducer later. handle submit and handle form data implemented in eventform

--- src/components/events/EventForm.jsx
+++ src/components/events/EventForm.jsx
@@ -1,19 +1,36 @@
 import React, {useState} from 'react';
 import {useGlobalState} from 'utils/stateContext';
+import {useNavigate} from 'react-router-dom';
 
-function EventForm() {
+function EventForm({addEvent}) {
   const initialEventData = {
-    date: Date.new(),
     start: Date.new(),
     end: Date.new(),
     truck: ''
   };
 
   const [formData, setFormData] = useState(initialEventData);
-
+  const navigate = useNavigate();
   const {store} = useGlobalState();
   const {foodTrucks} = store;
   console.log(foodTrucks);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.start === '' || formData.end === '' || formData.truck === '') {
+      return console.log("Please don't leave an empty field");
+    } else {
+      addEvent(formData.start, formData.end, formData.truck);
+      navigate('/events');
+    }
+  };
+
+  const handleFormData = (event) => {
+    setFormData({
+      ...formData, // previous state
+      [event.target.name]: event.target.value // new state
+    });
+  };
 
   return (
     <div>
