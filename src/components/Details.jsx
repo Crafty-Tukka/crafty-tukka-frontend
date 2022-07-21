0eeafc17b2ff10e25e21488fb20725891a8ccf1e
jwsandeman <jw.sandeman@gmail.com>
2022-07-21 21:34:45 +1000

    updated conditional logic and mui styling in details card and tile card. added formatted date property to events seed data.

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ComputerIcon from '@mui/icons-material/Computer';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 import {
   Button,
   Card,
@@ -9,8 +10,11 @@ import {
   CardMedia,
   Typography,
   Link,
-  Chip
+  Chip,
+  CardHeader
 } from '@mui/material';
+import LinkedCard from './LinkedCard';
+import {useGlobalState} from 'utils/stateContext';
 
 const style = {
   position: 'absolute',
@@ -18,7 +22,8 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 640,
-  height: 640,
+  height: 720,
+  overflow: 'auto',
   // bgcolor: 'background.paper',
   // border: '2px solid #000',
   // maxWidth: 345,
@@ -27,43 +32,93 @@ const style = {
 };
 
 function Details({item, imgPath, handleClose}) {
+  const {store} = useGlobalState();
+  const {confirmedEvents} = store; // this will need to be changed to item.events
+
   return (
     <>
       <Card sx={style}>
         <CardMedia component="img" height="340" image={imgPath} alt={item.name} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {item.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {item.description}
-          </Typography>
-          {item.start ? <Typography variant="h6">Start time: {item.start}</Typography> : null}
-          {item.finish ? <Typography variant="h6">Finish time: {item.finish}</Typography> : null}
-          {item.location ? (
-            <Typography variant="body2">
-              {item.location.address1} <br /> {item.location.city}
-            </Typography>
-          ) : null}
-          {item.email ? <Typography variant="h6">Email: {item.email}</Typography> : null}
-          {item.mobile ? <Typography variant="h6">Mobile: {item.mobile}</Typography> : null}
-          <span>
+        <CardHeader title={item.name} subheader={item.date ? item.date : item.website} />
+        {!item.date ? (
+          <CardContent sx={{width: '50%', pt: 0, pb: 1}}>
             {item.facebook ? (
-              <Link href={item.facebook} target="_blank" rel="noreferrer">
-                <FacebookIcon />
+              <Link sx={{mt: 1, pr: 4}} href={item.facebook} target="_blank" rel="noreferrer">
+                <FacebookOutlinedIcon sx={{margin: 'auto'}} />
               </Link>
             ) : null}
             {item.website ? (
-              <Link href={item.website} target="_blank" rel="noreferrer">
+              <Link sx={{pr: 4}} href={item.website} target="_blank" rel="noreferrer">
                 <ComputerIcon />
               </Link>
             ) : null}
-          </span>
+            {item.google_maps ? (
+              <Link sx={{pr: 4}} href={item.google_maps} target="_blank" rel="noreferrer">
+                <MapOutlinedIcon />
+              </Link>
+            ) : null}
+            {item.email ? (
+              <Typography sx={{mt: 1}} variant="body2" color="text.secondary">
+                Email: {item.email}
+              </Typography>
+            ) : null}
+            {item.mobile ? (
+              <Typography variant="body2" color="text.secondary">
+                Mobile: {item.mobile}
+              </Typography>
+            ) : null}
+            {item.location ? (
+              <Typography variant="body2" color="text.secondary">
+                {item.location.address1}, {item.location.city}
+              </Typography>
+            ) : null}
+          </CardContent>
+        ) : null}
+        {item.category ? (
+          <CardActions>
+            <Chip
+              sx={{marginLeft: 1}}
+              size="small"
+              label={item.category}
+              color="secondary"
+              variant="outlined"
+            />
+          </CardActions>
+        ) : null}
+        <CardContent sx={{mt: 0, pt: 0}}>
+          <Typography variant="body" color="text.secondary">
+            {item.description}
+          </Typography>
+
+          {/* Upcoming Events */}
+          {!item.date ? (
+            <>
+              <Typography sx={{mt: 2}} variant="h6" component="div">
+                Upcoming Events
+              </Typography>
+              {confirmedEvents.map((event) => {
+                return event.confirmed_status === 'confirmed' ? (
+                  <LinkedCard key={event.id} imgPath={event.img} item={event}>
+                    {/* this can be refactored into preview card component */}
+                    <Typography component="div" variant="h6">
+                      {event.name}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary">
+                      {event.date}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" component="div">
+                      {event.description}
+                    </Typography>
+                    {/* <Typography variant="body2" color="text.secondary">
+                  {event.finish}
+                </Typography> */}
+                  </LinkedCard>
+                ) : null;
+              })}
+            </>
+          ) : null}
         </CardContent>
         <CardActions>
-          {item.category ? (
-            <Chip label={item.category} color="secondary" variant="outlined" />
-          ) : null}
           <Button size="small" onClick={handleClose}>
             Close
           </Button>
--- src/components/TileCard.jsx
+++ src/components/TileCard.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import {Card, CardActions, CardContent, CardMedia, Chip, Typography} from '@mui/material';
+import {
+  Card,
+  CardActions,
+  // CardContent,
+  CardHeader,
+  CardMedia,
+  Chip
+  // Typography
+} from '@mui/material';
 
 function TileCard({imgPath, item, children}) {
   return (
-    <Card sx={{maxWidth: 345, minWidth: 240, m: 2, minHeight: 300}}>
+    <Card sx={{width: 340, m: 2}}>
       <CardMedia component="img" alt="image" height="140" image={imgPath} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardHeader title={item.name} subheader={item.website && item.website} />
+      {/* <CardContent> */}
+      {/* <Typography gutterBottom variant="h5" component="div">
           {item.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
+        </Typography> */}
+      {/* <Typography sx={{margin: 0, overflow: 'hidden'}} variant="body2" color="text.secondary">
           {item.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        {item.category ? <Chip label={item.category} color="secondary" variant="outlined" /> : null}
+        </Typography> */}
+      {/* {item.email ? <Typography variant="body2">Email: {item.email}</Typography> : null}
+        {item.mobile ? <Typography variant="body2">Mobile: {item.mobile}</Typography> : null} */}
+      {/* </CardContent> */}
+      <CardActions sx={{marginBottom: 1, pt: 0, pl: 2}}>
+        {item.category ? (
+          <Chip size="small" label={item.category} color="secondary" variant="outlined" />
+        ) : null}
       </CardActions>
     </Card>
   );
