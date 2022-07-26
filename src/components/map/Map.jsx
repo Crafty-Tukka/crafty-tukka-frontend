31d5c41753201254a47581a82ebbe636893a210f
simon-tanna <gcab022109@coderacademy.edu.au>
2022-07-26 12:20:42 +1000

    commit before checkout

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,14 +1,14 @@
-import {useMemo, useState, useEffect} from 'react';
+import {useMemo, useState, useEffect, useRef, useCallback} from 'react';
 import {GoogleMap, Marker} from '@react-google-maps/api';
 import {useGlobalState} from 'utils/stateContext';
 import {getGeocode, getLatLng} from 'use-places-autocomplete';
 // import Events from 'components/events/Events';
 // import './Map.css';
-// import venues from '../../data/breweries.json';
+import venues from '../../data/breweries.json';
 
 function Map() {
-  const {store} = useGlobalState();
-  const {venues} = store;
+  // const {store} = useGlobalState();
+  // const {venues} = store;
   const initialMapPosition = {position: {lat: -27.4705, lng: 153.026}, initialZoom: 12.1};
   const options = useMemo(
     () => ({
@@ -21,14 +21,14 @@ function Map() {
   const [zoom, setZoom] = useState(initialMapPosition.initialZoom); // initial zoom: ;
   const [center, setCenter] = useState(initialMapPosition.position);
 
-  getGeocode(
-    venues.map((venue) => {
-      return `${venue.address1} ${venue.suburb}`;
-    })
-  ).then((results) => {
-    const {lat, lng} = getLatLng(results[0]);
-    console.log('Coordinates: ', {lat, lng});
-  });
+  // getGeocode(
+  //   venues.map((venue) => {
+  //     return `${venue.address1} ${venue.suburb}`;
+  //   })
+  // ).then((results) => {
+  //   const {lat, lng} = getLatLng(results[0]);
+  //   console.log('Coordinates: ', {lat, lng});
+  // });
 
   useEffect(() => {
     setMarkers(venues);
@@ -39,21 +39,21 @@ function Map() {
     setZoom(15);
   };
 
-  const [mapInstance, setMapInstance] = useState({});
-  const onLoad = (map) => {
-    setMapInstance(map);
-    console.log(map);
-  };
-
-  const onZoomChanged = () => {
-    setZoom(mapInstance.zoom);
-  };
-  // const mapRef = useRef(GoogleMap);
-  // const onLoad = useCallback((map) => (mapRef.current = map), []);
+  // const [mapInstance, setMapInstance] = useState({});
+  // const onLoad = (map) => {
+  //   setMapInstance(map);
+  //   console.log(map);
+  // };
 
   // const onZoomChanged = () => {
-  //   setZoom(mapRef.zoom);
+  //   setZoom(mapInstance.zoom);
   // };
+  const mapRef = useRef(GoogleMap);
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+
+  const onZoomChanged = () => {
+    setZoom(mapRef.zoom);
+  };
 
   return (
     // <div className="container">
@@ -71,7 +71,7 @@ function Map() {
       {markers.map((marker) => (
         <Marker
           key={marker.id}
-          position={marker.address1}
+          position={marker.position}
           onClick={onClick}
           getGeocode={getGeocode}
         />
